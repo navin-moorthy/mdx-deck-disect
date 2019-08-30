b9bc6d01fc44be66f40b1cb8e220fe3be3a38574
NavinNavi19 <navin007.a@gmail.com>
2019-08-30 12:22:35 +0000

    Adjust parsing in loader

--- cli.js
+++ cli.js
@@ -1,9 +1,49 @@
 #!/usr/bin/env node
 
+const meow = require("meow");
 const open = require("react-dev-utils/openBrowser");
+const chalk = require("chalk");
 const dev = require("./lib/dev");
 
-const opt = { port: 8080 };
+const config = require("pkg-conf").sync("mdx-deck");
+
+const log = (...arg) => {
+  console.log(chalk.magenta("[mdx-deck]"), ...arg);
+};
+
+log.error = (...arg) => {
+  console.log(chalk.red("[err]"), ...arg);
+};
+
+const cli = meow(
+  `
+  Usage
+    $ mdx-deck deck.mdx
+    $ mdx-deck build deck.mdx
+  Options
+    -p --port   Dev server port
+    --no-open   Prevent from opening in default browser
+  `,
+  {
+    flags: {
+      port: {
+        type: "string",
+        alias: "p"
+      },
+      open: {
+        type: "boolean",
+        alias: "o",
+        default: true
+      }
+    }
+  }
+);
+
+const [entry] = cli.input;
+
+if (!entry) cli.showHelp(0);
+
+const opt = Object.assign({ port: 8080, entry }, config, cli.flags);
 
 dev(opt)
   .then(res => {
